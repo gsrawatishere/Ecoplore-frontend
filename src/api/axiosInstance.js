b36e3b3e7342d33a477231a172739e6575c48070
gsrawatishe <gaurishankarrawat78@gmail.com>
2025-10-18 22:32:33 +0530

    api changes

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,9 +2,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-   baseURL: 'http://localhost:8080/api/v1',   
+  //  baseURL: 'http://localhost:8080/api/v1',   
 
-  //  baseURL: 'https://api.greenplore.com/api/v1', 
+   baseURL: 'https://api.greenplore.com/api/v1', 
   withCredentials: true
 });
 
